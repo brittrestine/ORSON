ff5a3824a0ae66747cc1074b69a45bad6ec6dc4d
Catherine Jean <catherinejeanbean@gmail.com>
2017-08-10 15:06:48 -0500

    Change route to root after sign in, instead of going to user/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,17 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def after_sign_in_path_for(resource)
+        root_path
+    end
+    
     protected
 
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :bio, :location, :email, :password) }
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:location, :bio, :email, :password, :current_password) }
-        end
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :bio, :location, :email, :password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:location, :bio, :email, :password, :current_password) }
+    end
+
 end
 
 
