30ba19cccd46957490db632093eb7092a3aaa641
Catherine Jean <catherinejeanbean@gmail.com>
2017-08-10 18:23:18 -0500

    Comment in Review model: needs to prevent auser- multiple  reviews on same movie

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :body, presence: true,
                     length: { minimum: 5 }
+#need uniqueness true on the user_id, so that a user can not make more than one review 
 end
