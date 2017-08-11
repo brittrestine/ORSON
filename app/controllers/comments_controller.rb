12525beef6e23ecb8219ff8abd0ec48855f12bfb
Catherine Jean <catherinejeanbean@gmail.com>
2017-08-11 15:53:00 -0500

    Make create comment work and display on the review section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,11 @@ class CommentsController < ApplicationController
 
   def create
     @review = Review.find(params[:review_id])
-    @comment = @review.comments.create(comment_params)
-    # redirect_to article_path(@article)
-      redirect_to '/review/new'
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.review = @review
+    @comment.save
+      redirect_to "/movies/" + @review.movie.to_s
   end
 
   def destroy
