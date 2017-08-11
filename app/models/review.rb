91ac8e15e99e491ab9e19461281b0a16b25714c8
Katie McNeil <katie.mcneil@live.com>
2017-08-11 16:29:33 -0500

    Create ajax capability for movie index display. Implement helper methods to display ratings and average ratings. Update view of index, movie page, etc.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
+
   has_many :comments, dependent: :destroy
   validates :body, presence: true,
                     length: { minimum: 5 }
+  validates_inclusion_of :rating, in: 1..5
 end
